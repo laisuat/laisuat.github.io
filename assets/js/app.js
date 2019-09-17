d5c1e5481a71d521a6360431df8a2aa5b5d0f395
nqloc <www.nqloc@gmail.com>
2019-09-17 23:09:49 +0700

    Tao ung ung

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,24 @@
 var app = angular.module('myApp', ['ui.router']);
 
-app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+	if(window.history && window.history.pushState){
+	    $locationProvider.html5Mode(true);
+	}
+
     $urlRouterProvider.otherwise('/');
  
     $stateProvider
         .state('home', {
             url:'/',
-            templateUrl: 'templates/home.html'
+            templateUrl: 'home.html'
         })
         .state('about', {
             url:'/about',
-            templateUrl: 'templates/about.html'
+            templateUrl: 'about.html'
         })
- }]);
+ });
 
 app.controller('myCtrl', function($scope) {
-    $scope.message = 'test';
-   
-    $scope.scotches = [
-        {
-            name: 'Macallan 12',
-            price: 50
-        },
-        {
-            name: 'Chivas Regal Royal Salute',
-            price: 10000
-        },
-        {
-            name: 'Glenfiddich 1937',
-            price: 20000
-        }
-    ];
+
 });
