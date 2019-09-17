c8a676c26c871a1a80649977a05ad9757b402ce0
nqloc <www.nqloc@gmail.com>
2019-09-17 22:34:55 +0700

    Tao ung ung

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,38 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        .state('home', {
+            url: '/',
+            templateUrl: 'home.html'
+        })
+        
+        .state('about', {
+            url: '/about',
+            templateUrl: 'about.html'
+        });
+        
+});
 
 app.controller('myCtrl', function($scope) {
-    $scope.name = "John Doe";
+    $scope.message = 'test';
+   
+    $scope.scotches = [
+        {
+            name: 'Macallan 12',
+            price: 50
+        },
+        {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name: 'Glenfiddich 1937',
+            price: 20000
+        }
+    ];
 });
